182d3e298f4ffa8027c3c0d4e9f5b624e2c149ee
akagisho <akagisho@gmail.com>
2015-02-07 16:18:21 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ps_log
+
+logging processes' history for Linux
+
+## Usage
+
+Create directory.
+
+    $ mkdir /path/to/log_dir
+
+Set cron.
+
+    */3 * * * * cd /path/to/log_dir && bash /path/to/ps_log.sh
---
+++ ps_log.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
+unalias -a
+
+DATE=`date +"%Y%m%d"`
+TIME=`date +"%Y%m%d_%H%M"`
+DIR=$DATE
+PID_FILE=ps_log.pid
+LOG_GENERATION=${LOG_GENERATION:-14}
+
+function ERROR_EXIT {
+  echo $@ 1>&2
+  exit 1
+}
+
+function CHECK_DUPLICATION {
+  if [ -f $PID_FILE ]; then
+    ps --pid `head -1 $PID_FILE` > /dev/null \
+      && ERROR_EXIT "another process is running"
+  fi
+
+  echo $$ > $PID_FILE
+}
+
+function EXEC {
+  local COMMAND=$@
+  local FILE=$DIR/${1}_$TIME.log
+  nice $COMMAND > $FILE 2>&1
+}
+
+function LOG_ROTATE {
+  local num=`ls | sort -n | grep -cE '^[0-9]{8}$'`
+  if [ $num -gt $LOG_GENERATION ]; then
+    local del=`expr $num - $LOG_GENERATION`
+    for i in `ls | sort -n | grep -E '^[0-9]{8}$' | head -n $del`; do
+      rm -rf $i
+    done
+  fi
+}
+
+mkdir -p $DIR
+
+CHECK_DUPLICATION
+
+EXEC df
+EXEC ps -eflm
+EXEC vmstat
+EXEC netstat -an
+EXEC iostat
+EXEC top -b -n 1
+EXEC free
+EXEC w
+
+LOG_ROTATE
